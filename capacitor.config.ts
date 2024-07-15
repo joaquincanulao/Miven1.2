9cb916ab15c69ea9520fcad233c81439356db8e2
jcanulao <jcanulao@admin-pc.diresandom.armada.cl>
2024-07-15 15:07:27 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miven1.1',
+  webDir: 'www'
+};
+
+export default config;
